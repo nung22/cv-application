ba7c683e9969944d0fdb3bc654f54545133d25e3
Nicholas Ung <ung.nicholask@gmail.com>
2023-09-10 23:48:59 -0700

    Add resume template

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Button from '@mui/material/Button';
+import Paper from '@mui/material/Paper';
+import Header from './components/Header'
 import './App.css'
 
 function App() {
@@ -8,26 +9,28 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='flex justify-center gap-7'>
+        <div>
+          <h1 className='text-4xl font-bold mb-8'>CV Application</h1>
+          <div className="">
+            <Button variant="contained" onClick={() => setCount((count) => count + 1)}>
+              count is {count}
+            </Button>
+            <Button variant="outlined" style={{marginLeft: 5, color: "red"}} onClick={() => setCount(0)}>
+              Reset
+            </Button>
+            <p>
+              Edit <code>src/App.jsx</code> and save to test HMR
+            </p>
+          </div>
+        </div>
+        <Paper square elevation={8} className='' style={{
+          minWidth: "50rem",
+          minHeight: "70rem"
+        }}>
+          <Header/>
+        </Paper>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
